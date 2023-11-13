ed9647c2463fb2c085e8a3ed9527c5a1908e277a
aileennguyen02 <103895991+aileennguyen02@users.noreply.github.com>
2023-11-13 14:02:27 -0500

    Update TopView.m

--- TopView.m
+++ TopView.m
@@ -1,13 +1,11 @@
 clc; clear;
 
-%% Calibration
+%% Calibration - might not need this section tbh
 
 % import calibration image
 prompt = "What is the filename of the top view calibration image?  ";
 filename = input(prompt,"s");
 img = imread(filename);
-
-BW = im2bw(import,.4); %chose an arbitrary value for level for now, will need to test
 
 %% Top View Measurement
 
@@ -15,3 +13,22 @@ BW = im2bw(import,.4); %chose an arbitrary value for level for now, will need to
 prompt = "What is the filename of the top view image with the nerve wrap?  ";
 filename = input(prompt,"s");
 img = imread(filename);
+imshow(img);
+% now ideally we have img of full pseudo nerve with wrap draped over
+% wrap should not be longer than length of pseudonerve
+
+h = imdistline; % creates a draggable distance tool 
+% adjust line to capture full length of pseudo nerve (PN)
+
+%% With this tool, we may have to run code section by section
+prompt = "What is the length of the pseudo nerve in pixels?  ";
+length_px = input(prompt,"s");
+scale = 8/length_px; %replace 8 w/ length of PN, keep units consistant
+imshow(img);
+h = imdistline;
+
+%% Now run imdistline to get max width of nerve wrap according to top view
+prompt = "What is the maximum width of the nerve wrap?  ";
+width_px = input(prompt,"s");
+width_cm = width_px * scale; %I'm assuming cm, but we can adjust units accordingly
+
